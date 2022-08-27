1ed00544a4f07537ef91188cb49ab2a87bda6e45
Vinicius Savitraz <vinicius.eduardo@rpinfo.com.br>
2022-08-27 18:55:22 -0300

    Removendo import sem uso

--- src/main/java/com/ufpr/br/cmmsufpr/incident/IncidentController.java
+++ src/main/java/com/ufpr/br/cmmsufpr/incident/IncidentController.java
@@ -1,6 +1,5 @@
 package com.ufpr.br.cmmsufpr.incident;
 
-import com.ufpr.br.cmmsufpr.database.DatabaseConnection;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
